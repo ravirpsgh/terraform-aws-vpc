dc570d56c9c902d51d777b815940ad0bc934c103
ravirpsgh <144752190+ravirpsgh@users.noreply.github.com>
2025-04-14 20:36:44 +0530

    Create outputs.tf
    subnet details updated

---
+++ outputs.tf
@@ -0,0 +1,20 @@
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  description = "List of private subnet IDs"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "List of public subnet IDs"
+  value       = module.vpc.public_subnets
+}
+
+output "nat_public_ips" {
+  description = "Public IPs of NAT gateways"
+  value       = module.vpc.nat_public_ips
+  sensitive   = true
+}
